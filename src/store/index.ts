478fc1f6651359040ce73a572ef3f77a8c351db1
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-09-14 20:58:37 -0300

    primeiro commit

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { createPinia, defineStore } from 'pinia'
+
+
+export const store = defineStore('Store', {
+    state: () => {
+         return {
+
+         }
+    },
+    actions: {
+
+    },
+    getters: {
+        
+    }
+
+})
