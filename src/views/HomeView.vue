5857487a48d04e25d72edebc4eda8830bb69018c
gilbertogabriel <gilberto.gabriel.dev@gmail.com>
2023-10-30 18:09:11 -0300

    :sparkled:New card component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TransactionCardComp value="200,00" date="28/03/2023" transaction-name="Magalu" type="receita" hour="12:20"></TransactionCardComp>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {defineComponent} from 'vue';
+import TransactionCardComp from "@/components/TransactionCardComp.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    TransactionCardComp,
   },
 });
 </script>
