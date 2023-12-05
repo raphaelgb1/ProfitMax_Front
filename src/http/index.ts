cfb635d79e7338462ba9b393f9e6484829575bbe
raphaelgb1 <raphaelgb20@gmail.com>
2023-12-05 10:31:13 -0300

    Adjusting axios

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,11 +1,28 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 const httpClient: AxiosInstance = axios.create({
-  // baseURL: "http://192.168.4.21:3001/",
   baseURL: "http://localhost:3001/",
   headers: {
     "Content-type": "application/json",
   },
 });
+
+// Tratamento global de erros
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response) {
+      // O servidor respondeu com um status de erro (4xx ou 5xx)
+      console.error("Erro de resposta do servidor:", error.response.data);
+    } else if (error.request) {
+      // A solicitação foi feita, mas não houve resposta do servidor
+      console.error("Sem resposta do servidor:", error.request);
+    } else {
+      // Algo aconteceu durante a configuração da solicitação que provocou um erro
+      console.error("Erro durante a configuração da solicitação:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default httpClient;
