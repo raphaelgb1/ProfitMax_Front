45242adbf693653cd3886172a081748ee21606b0
raphaelgb1 <raphaelgb20@gmail.com>
2023-12-09 20:39:32 -0300

    Adjusting closeOnCLick

--- src/entyti/Toastr.ts
+++ src/entyti/Toastr.ts
@@ -4,7 +4,7 @@ class Toastr {
     private readonly toast = useToast();
     private readonly options: PluginOptions = {
         timeout: 2038,
-        closeOnClick: true,
+        closeOnClick: false,
         position: POSITION.TOP_RIGHT,
         pauseOnFocusLoss: true,
         pauseOnHover: true,
